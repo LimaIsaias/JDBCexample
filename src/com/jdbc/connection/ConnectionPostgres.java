6ae99ebb39d1052fe84bde810763777d3b5019ca
Isaias Lima <coelholimaisaias@gmail.com>
2018-01-22 21:36:01 -0200

    Successfully connected!

--- src/com/jdbc/connection/ConnectionPostgres.java
+++ src/com/jdbc/connection/ConnectionPostgres.java
@@ -1,5 +1,31 @@
 package com.jdbc.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionPostgres {
 
+	public static void main(String[] args) {
+		Connection connection = null;
+		try {
+			String url = "jdbc:postgresql://localhost:5432/contacts";
+			String user = "postgres";
+			String password = "estagio";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			System.out.println("Successfully connected!");
+		} catch (Exception e) {
+			System.out.println("Error during catch block : " + e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error during finally block : " + e.getMessage());
+			}
+		}
+
+	}
 }
